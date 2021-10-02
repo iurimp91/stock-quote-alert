34416ebe377b9bfa454b3b798f55c8c5eb88911b
Iuri Magnago <iurimp91@gmail.com>
2021-10-02 13:33:50 -0300

    feat: get data from api

--- src/StockQuoteAlert/Program.cs
+++ src/StockQuoteAlert/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
 
 namespace StockQuoteAlert
 {
@@ -6,6 +9,15 @@ namespace StockQuoteAlert
     {
         static void Main(string[] args)
         {
+            string QUERY_URL = "https://api.hgbrasil.com/finance/stock_price?key=f626405b&symbol=bidi4";
+            Uri queryUri = new Uri(QUERY_URL);
+
+            using (WebClient client = new WebClient())
+            {   
+                var data = client.DownloadString(queryUri);
+                System.Console.WriteLine(data);
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
